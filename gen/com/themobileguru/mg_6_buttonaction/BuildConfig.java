3314651e812c5a37ce4eab324c03b54eb22642b6
themobileguruz <themobileguruz@gmail.com>
2015-03-02 17:29:40 +0530

    Example 5 :Button Widget
    This is a simple piece of code to explain how to do click operation with
    Button by Implementing OnLongClickListener Method

---
+++ gen/com/themobileguru/mg_6_buttonaction/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.themobileguru.mg_6_buttonaction;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
